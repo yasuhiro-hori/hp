714cd3f552c0bfce6f1ef4e38bd7a024e1996fbb
yasuhiro-hori-se <>
2026-01-31 07:05:59 +0900

    fix

--- front/next.config.ts
+++ front/next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   reactStrictMode: false,
   output: 'export',
   images: { unoptimized: true },
-  eslint: {
-    ignoreDuringBuilds: false,
-    dirs: ["src"],
-  },
 }
 
 export default nextConfig
