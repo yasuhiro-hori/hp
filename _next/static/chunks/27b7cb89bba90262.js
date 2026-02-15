f075cf2c40f225281997d5e80ac234bf67b1b1f8
yasuhiro-hori-se <>
2026-02-15 12:09:24 +0900

    Updates

--- _next/static/chunks/27b7cb89bba90262.js
+++
@@ -1,7 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/5c7463836c3731b4.js",
-  "static/chunks/04d26e32acc14b9b.js",
-  "static/chunks/8c4da650ff6dcddb.js",
-  "static/chunks/acf2629177e1f4f5.css",
-  "static/chunks/turbopack-1c78557f89954ead.js"
-])
--- _next/static/chunks/fbc6f38ba12a3198.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/d2ddce90d3854ea5.js",
-  "static/chunks/04d26e32acc14b9b.js",
-  "static/chunks/8c4da650ff6dcddb.js",
-  "static/chunks/turbopack-76509aec93011ff3.js"
-])
