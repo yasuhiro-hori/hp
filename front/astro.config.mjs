48e393ae2e79edb91d4c2bb8cec34754aeb14d23
yasuhiro-hori-se <>
2026-02-15 12:25:31 +0900

    fix

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://tecsiz.co.jp',
     integrations: [react()],
     output: 'static',
     build: {
