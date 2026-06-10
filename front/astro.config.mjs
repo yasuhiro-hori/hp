9ec557b6eca3d211efe1e26589a9315d71210c66
yasuhiro-hori <yasuhiro-hori@users.noreply.github.com>
2026-06-10 16:57:07 +0900

    fix

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -5,7 +5,12 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tecsiz.co.jp',
-    integrations: [react(), sitemap()],
+    integrations: [
+        react(),
+        sitemap({
+            filter: (page) => !page.includes('/privacy/'),
+        }),
+    ],
     output: 'static',
     build: {
         format: 'directory'
